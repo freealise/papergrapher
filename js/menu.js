4ac6463665d77810f4904897a64c509330f22163
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-03-26 21:43:13 +0300

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -120,7 +120,7 @@ pg.menu = function() {
 		});
 		
 		jQuery('.traceImage_button').click(function () {
-			jQuery('#tracerIframe').removeClass('hidden');
+			jQuery('#tracerIframe').toggleVisibility();
 			/*var url = prompt("Paste URL to SVG", "http://");
 			if (url) {
 				pg.import.importAndAddSVG(url);
