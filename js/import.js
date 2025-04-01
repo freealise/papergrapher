5f62749ec38bf13f82a28982c38969871ed377a1
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-04-02 02:33:15 +0300

    Update import.js

--- js/import.js
+++ js/import.js
@@ -78,7 +78,7 @@ pg.import = function () {
 							j++;
 						}
 					}
-					paths[i].simplify(0.001);
+					paths[i].simplify(0.0);
 					for (var j=0; j<paths[i].segments.length; j++) {
 						if (corners[j] === true) {
 							paths[i].segments[j].clearHandles();
