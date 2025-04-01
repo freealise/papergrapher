fe074f96d8cd0abb04733033fcbb8ec7298904af
Marina Goritskaia <5275747+freealise@users.noreply.github.com>
2025-04-01 20:28:44 +0300

    Update import.js

--- js/import.js
+++ js/import.js
@@ -54,7 +54,7 @@ pg.import = function () {
 						paths[i].segments[j].point.y = y/w;
 					}
 					paths[i].smooth({ type: 'continuous' });
-					paths[i].simplify(0.0625);
+					paths[i].simplify(0.125);
 				}
 			} catch(e) {alert(e);}
 		 }
